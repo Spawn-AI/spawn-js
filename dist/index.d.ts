4dab1cac1db8e3ea09ce8bd0ab361b57a5ffaf6a
Benjamin Trom <benjamintrom@gmail.com>
2023-01-08 13:30:16 +0100

    prod as the default branch + subscribe to training

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,3 @@
-import * as _supabase_supabase_js from '@supabase/supabase-js';
 import { SupabaseClient } from '@supabase/supabase-js';
 
 type WorkerFilter = {
@@ -7,6 +6,18 @@ type WorkerFilter = {
     branch?: string;
     is_dirty?: boolean;
     cluster?: number;
+};
+type TrainingImage = {
+    url: string;
+    label: string;
+};
+type PatchTrainerConfig = {
+    dataset: any[];
+    patch_name: string;
+    description: string;
+    learning_rate: number;
+    steps: number;
+    rank: number;
 };
 type PatchConfig = {
     name: string;
@@ -44,36 +55,34 @@ declare class SelasClient {
     add_ons: any[];
     app_user_id: string;
     constructor(supabase: SupabaseClient, app_id: string, key: string, app_user_external_id: string, app_user_token: string, worker_filter?: WorkerFilter);
-    setUserID: () => Promise<void>;
-    handle_error: (error: any) => void;
-    test_connection: () => Promise<void>;
+    private handle_error;
     private rpc;
+    test_connection: () => Promise<void>;
     getServiceList: () => Promise<any[] | null>;
     getAddOnList: () => Promise<any[] | null>;
-    echo: (args: {
-        message: string;
-    }) => Promise<any[] | null>;
+    setUserID: () => Promise<void>;
+    echo: (message: string) => Promise<any[] | null>;
     getAppUserCredits: () => Promise<any[] | null>;
-    getAppUserJobHistory: (args: {
-        limit: number;
-        offset: number;
+    getAppUserJobHistory: (limit: number, offset: number) => Promise<any[] | null>;
+    getServiceConfigCost: (service_name: string, job_config: string) => Promise<any[] | null>;
+    private postJob;
+    getResult: (job_id: string) => Promise<any[] | null>;
+    subscribeToJob: (job_id: string, callback: (result: object) => void) => Promise<void>;
+    costStableDiffusion: (prompt: string, args?: {
+        service_name?: string;
+        steps?: number;
+        skip_steps?: number;
+        batch_size?: 1 | 2 | 4 | 8 | 16;
+        sampler?: "plms" | "ddim" | "k_lms" | "k_euler" | "k_euler_a" | "dpm_multistep";
+        guidance_scale?: number;
+        width?: 384 | 448 | 512 | 575 | 768 | 640 | 704 | 768;
+        height?: 384 | 448 | 512 | 575 | 768 | 640 | 704 | 768;
+        negative_prompt?: string;
+        image_format?: "png" | "jpeg" | "avif" | "webp";
+        translate_prompt?: boolean;
+        nsfw_filter?: boolean;
+        patches?: PatchConfig[];
     }) => Promise<any[] | null>;
-    postJob: (args: {
-        service_name: string;
-        job_config: object;
-    }) => Promise<{
-        data: any[] | null;
-        error: _supabase_supabase_js.PostgrestError | null;
-    }>;
-    subscribeToJob: (args: {
-        job_id: string;
-        callback: (result: object) => void;
-    }) => Promise<void>;
-    getServiceConfigCost: (args: {
-        service_name: string;
-        job_config: string;
-    }) => Promise<any[] | null>;
-    private patchConfigToAddonConfig;
     runStableDiffusion: (prompt: string, args?: {
         service_name?: string;
         steps?: number;
@@ -88,10 +97,26 @@ declare class SelasClient {
         translate_prompt?: boolean;
         nsfw_filter?: boolean;
         patches?: PatchConfig[];
-    }) => Promise<{
-        data: any[] | null;
-        error: _supabase_supabase_js.PostgrestError | null;
-    }>;
+    }) => Promise<any[] | null>;
+    private patchConfigToAddonConfig;
+    costPatchTrainer: (dataset: TrainingImage[], patch_name: string, args?: {
+        service_name?: string;
+        description?: string;
+        learning_rate?: number;
+        steps?: number;
+        rank?: number;
+    }) => Promise<any[] | null>;
+    runPatchTrainer: (dataset: TrainingImage[], patch_name: string, args?: {
+        service_name?: string;
+        description?: string;
+        learning_rate?: number;
+        steps?: number;
+        rank?: number;
+    }) => Promise<any[] | null>;
+    shareAddOn: (add_on_name: string, app_user_external_id: string) => Promise<any[] | null>;
+    deleteAddOn: (add_on_name: string) => Promise<any[] | null>;
+    renameAddOn: (add_on_name: string, new_add_on_name: string) => Promise<any[] | null>;
+    getCountActiveWorker: () => Promise<any[] | null>;
 }
 declare const createSelasClient: (credentials: {
     app_id: string;
@@ -100,4 +125,4 @@ declare const createSelasClient: (credentials: {
     app_user_token: string;
 }, worker_filter?: WorkerFilter) => Promise<SelasClient>;
 
-export { PatchConfig, SelasClient, StableDiffusionConfig, WorkerFilter, createSelasClient, createSelasClient as default };
+export { PatchConfig, PatchTrainerConfig, SelasClient, StableDiffusionConfig, TrainingImage, WorkerFilter, createSelasClient, createSelasClient as default };
--- dist/index.mjs
+++ dist/index.mjs
@@ -11,23 +11,11 @@ function PatchConfig(name, alpha_text_encoder, alpha_unet, steps) {
 }
 class SelasClient {
   constructor(supabase, app_id, key, app_user_external_id, app_user_token, worker_filter) {
-    this.setUserID = async () => {
-      const { data, error } = await this.supabase.rpc("app_user_get_id", {
-        p_app_id: this.app_id,
-        p_key: this.key,
-        p_app_user_external_id: this.app_user_external_id,
-        p_app_user_token: this.app_user_token
-      });
-      if (error) {
-        throw new Error(error.message);
-      }
-      if (data) {
-        this.app_user_id = String(data);
-      }
-    };
     this.handle_error = (error) => {
       if (error.code === "") {
-        throw new Error("The database cannot be reached. Contact the administrator.");
+        throw new Error(
+          "The database cannot be reached. Contact the administrator."
+        );
       }
       if (error.message === "Invalid API key") {
         throw new Error("The API key is invalid. Contact the administrator.");
@@ -37,17 +25,6 @@ class SelasClient {
       }
       if (error.code === "P0001") {
         throw new Error(error.message);
-      }
-    };
-    this.test_connection = async () => {
-      const { data, error } = await this.rpc("app_user_echo", { message_app_user: "check" });
-      if (error) {
-        this.handle_error(error);
-      }
-      if (data) {
-        if (String(data) !== "check") {
-          throw new Error("There is a problem with the database. Contact the administrator.");
-        }
       }
     };
     this.rpc = async (fn, params) => {
@@ -60,6 +37,21 @@ class SelasClient {
       };
       const { data, error } = await this.supabase.rpc(fn, paramsWithToken);
       return { data, error };
+    };
+    this.test_connection = async () => {
+      const { data, error } = await this.rpc("app_user_echo", {
+        message_app_user: "check"
+      });
+      if (error) {
+        this.handle_error(error);
+      }
+      if (data) {
+        if (String(data) !== "check") {
+          throw new Error(
+            "There is a problem with the database. Contact the administrator."
+          );
+        }
+      }
     };
     this.getServiceList = async () => {
       const { data, error } = await this.rpc("app_user_get_services", {});
@@ -81,8 +73,24 @@ class SelasClient {
       }
       return data;
     };
-    this.echo = async (args) => {
-      const { data, error } = await this.rpc("app_user_echo", { message_app_user: args.message });
+    this.setUserID = async () => {
+      const { data, error } = await this.supabase.rpc("app_user_get_id", {
+        p_app_id: this.app_id,
+        p_key: this.key,
+        p_app_user_external_id: this.app_user_external_id,
+        p_app_user_token: this.app_user_token
+      });
+      if (error) {
+        throw new Error(error.message);
+      }
+      if (data) {
+        this.app_user_id = String(data);
+      }
+    };
+    this.echo = async (message) => {
+      const { data, error } = await this.rpc("app_user_echo", {
+        message_app_user: message
+      });
       if (error) {
         this.handle_error(error);
       }
@@ -95,79 +103,91 @@ class SelasClient {
       }
       return data;
     };
-    this.getAppUserJobHistory = async (args) => {
+    this.getAppUserJobHistory = async (limit, offset) => {
       const { data, error } = await this.rpc("app_user_get_job_history_detail", {
-        p_limit: args.limit,
-        p_offset: args.offset
+        p_limit: limit,
+        p_offset: offset
       });
       if (error) {
         this.handle_error(error);
       }
       return data;
     };
-    this.postJob = async (args) => {
-      const service = this.services.find((service2) => service2.name === args.service_name);
+    this.getServiceConfigCost = async (service_name, job_config) => {
+      const service_id = this.services.find(
+        (service) => service.name === service_name
+      )["id"];
+      if (!service_id) {
+        throw new Error("Invalid model name");
+      }
+      const { data, error } = await this.supabase.rpc(
+        "get_service_config_cost_client",
+        { p_service_id: service_id, p_config: job_config }
+      );
+      if (error) {
+        this.handle_error(error);
+      }
+      return data;
+    };
+    this.postJob = async (service_name, job_config) => {
+      const service = this.services.find(
+        (service2) => service2.name === service_name
+      );
       if (!service) {
         throw new Error("Invalid model name");
       }
       const { data, error } = await this.rpc("post_job", {
         p_service_id: service["id"],
-        p_job_config: JSON.stringify(args.job_config),
+        p_job_config: JSON.stringify(job_config),
         p_worker_filter: this.worker_filter
-      });
-      return { data, error };
-    };
-    this.subscribeToJob = async (args) => {
-      const client = new Pusher("ed00ed3037c02a5fd912", {
-        cluster: "eu"
-      });
-      const channel = client.subscribe(`job-${args.job_id}`);
-      channel.bind("result", args.callback);
-    };
-    this.getServiceConfigCost = async (args) => {
-      const service_id = this.services.find((service) => service.name === args.service_name)["id"];
-      if (!service_id) {
-        throw new Error("Invalid model name");
-      }
-      const { data, error } = await this.supabase.rpc("get_service_config_cost_client", {
-        p_service_id: service_id,
-        p_config: args.job_config
       });
       if (error) {
         this.handle_error(error);
       }
       return data;
     };
-    this.patchConfigToAddonConfig = (patch_config) => {
-      return {
-        id: this.add_ons.find((add_on) => add_on.name === patch_config.name).id,
-        config: {
-          alpha_unet: patch_config.alpha_unet,
-          alpha_text_encoder: patch_config.alpha_text_encoder,
-          steps: patch_config.steps
-        }
-      };
+    this.getResult = async (job_id) => {
+      const { data, error } = await this.rpc("app_user_get_job_result", {
+        p_job_id: job_id
+      });
+      if (error) {
+        this.handle_error(error);
+      }
+      return data;
     };
-    this.runStableDiffusion = async (prompt, args) => {
+    this.subscribeToJob = async (job_id, callback) => {
+      const client = new Pusher("ed00ed3037c02a5fd912", {
+        cluster: "eu"
+      });
+      const channel = client.subscribe(`job-${job_id}`);
+      channel.bind("result", callback);
+    };
+    this.costStableDiffusion = async (prompt, args) => {
       const service_name = args?.service_name || "stable-diffusion-2-1-base";
-      if (!this.services.find((service) => service.name === service_name)) {
+      if (!this.services.find((service2) => service2.name === service_name)) {
         throw new Error(`The service ${service_name} does not exist`);
       }
-      const service_interface = this.services.find((service) => service.name === service_name).interface;
+      const service_interface = this.services.find(
+        (service2) => service2.name === service_name
+      ).interface;
       if (service_interface !== "stable-diffusion") {
-        throw new Error(`The service ${service_name} does not have the stable-diffusion interface`);
+        throw new Error(
+          `The service ${service_name} does not have the stable-diffusion interface`
+        );
       }
       for (const patch of args?.patches || []) {
         if (!this.add_ons.find((add_on) => add_on.name === patch.name)) {
           throw new Error(`The add-on ${patch.name} does not exist`);
         }
-        let service = this.add_ons.find((add_on) => add_on.name === patch.name).service_name;
-        console.log(service);
         if (!this.add_ons.find((add_on) => add_on.name === patch.name).service_name.includes(service_name)) {
-          throw new Error(`The service ${service_name} does not have the add-on ${patch.name}`);
+          throw new Error(
+            `The service ${service_name} does not have the add-on ${patch.name}`
+          );
         }
       }
-      let add_ons = args?.patches?.map((patch) => this.patchConfigToAddonConfig(patch));
+      let add_ons = args?.patches?.map(
+        (patch) => this.patchConfigToAddonConfig(patch)
+      );
       const config = {
         steps: args?.steps || 28,
         skip_steps: args?.skip_steps || 0,
@@ -183,18 +203,225 @@ class SelasClient {
         nsfw_filter: args?.nsfw_filter || false,
         add_ons
       };
-      const response = await this.postJob({
-        service_name,
-        job_config: config
-      });
+      const service = this.services.find(
+        (service2) => service2.name === service_name
+      );
+      if (!service) {
+        throw new Error("Invalid model name");
+      }
+      const { data, error } = await this.supabase.rpc(
+        "get_service_config_cost_client",
+        { p_service_id: service["id"], p_config: JSON.stringify(config) }
+      );
+      if (error) {
+        this.handle_error(error);
+      }
+      return data;
+    };
+    this.runStableDiffusion = async (prompt, args) => {
+      const service_name = args?.service_name || "stable-diffusion-2-1-base";
+      if (!this.services.find((service) => service.name === service_name)) {
+        throw new Error(`The service ${service_name} does not exist`);
+      }
+      const service_interface = this.services.find(
+        (service) => service.name === service_name
+      ).interface;
+      if (service_interface !== "stable-diffusion") {
+        throw new Error(
+          `The service ${service_name} does not have the stable-diffusion interface`
+        );
+      }
+      for (const patch of args?.patches || []) {
+        if (!this.add_ons.find((add_on) => add_on.name === patch.name)) {
+          throw new Error(`The add-on ${patch.name} does not exist`);
+        }
+        let service = this.add_ons.find(
+          (add_on) => add_on.name === patch.name
+        ).service_name;
+        console.log(service);
+        if (!this.add_ons.find((add_on) => add_on.name === patch.name).service_name.includes(service_name)) {
+          throw new Error(
+            `The service ${service_name} does not have the add-on ${patch.name}`
+          );
+        }
+      }
+      let add_ons = args?.patches?.map(
+        (patch) => this.patchConfigToAddonConfig(patch)
+      );
+      const config = {
+        steps: args?.steps || 28,
+        skip_steps: args?.skip_steps || 0,
+        batch_size: args?.batch_size || 1,
+        sampler: args?.sampler || "k_euler",
+        guidance_scale: args?.guidance_scale || 10,
+        width: args?.width || 512,
+        height: args?.height || 512,
+        prompt: prompt || "banana in the kitchen",
+        negative_prompt: args?.negative_prompt || "ugly",
+        image_format: args?.image_format || "jpeg",
+        translate_prompt: args?.translate_prompt || false,
+        nsfw_filter: args?.nsfw_filter || false,
+        add_ons
+      };
+      const response = await this.postJob(service_name, config);
       return response;
+    };
+    this.patchConfigToAddonConfig = (patch_config) => {
+      return {
+        id: this.add_ons.find((add_on) => add_on.name === patch_config.name).id,
+        config: {
+          alpha_unet: patch_config.alpha_unet,
+          alpha_text_encoder: patch_config.alpha_text_encoder,
+          steps: patch_config.steps
+        }
+      };
+    };
+    this.costPatchTrainer = async (dataset, patch_name, args) => {
+      const service_name = args?.service_name || "patch_trainer_v1";
+      if (!this.services.find((service2) => service2.name === service_name)) {
+        throw new Error(`The service ${service_name} does not exist`);
+      }
+      const service_interface = this.services.find(
+        (service2) => service2.name === service_name
+      ).interface;
+      if (service_interface !== "train-patch-stable-diffusion") {
+        throw new Error(
+          `The service ${service_name} does not have the train-patch-stable-diffusion interface`
+        );
+      }
+      await this.getAddOnList();
+      if (!this.add_ons.find((add_on) => add_on.name === patch_name)) {
+        throw new Error(`The add-on ${patch_name} does not exist`);
+      }
+      const trainerConfig = {
+        dataset,
+        patch_name,
+        description: args?.description || "",
+        learning_rate: args?.learning_rate || 1e-4,
+        steps: args?.steps || 100,
+        rank: args?.rank || 4
+      };
+      const service = this.services.find(
+        (service2) => service2.name === service_name
+      );
+      if (!service) {
+        throw new Error("Invalid model name");
+      }
+      const { data, error } = await this.supabase.rpc(
+        "get_service_config_cost_client",
+        { p_service_id: service["id"], p_config: JSON.stringify(trainerConfig) }
+      );
+      if (error) {
+        this.handle_error(error);
+      }
+      return data;
+    };
+    this.runPatchTrainer = async (dataset, patch_name, args) => {
+      const service_name = args?.service_name || "patch_trainer_v1";
+      if (!this.services.find((service) => service.name === service_name)) {
+        throw new Error(`The service ${service_name} does not exist`);
+      }
+      const service_interface = this.services.find(
+        (service) => service.name === service_name
+      ).interface;
+      if (service_interface !== "train-patch-stable-diffusion") {
+        throw new Error(
+          `The service ${service_name} does not have the train-patch-stable-diffusion interface`
+        );
+      }
+      await this.getAddOnList();
+      if (this.add_ons.find((add_on) => add_on.name === patch_name)) {
+        throw new Error(`The add-on ${patch_name} already exists`);
+      }
+      let is_creating = await this.rpc("app_user_is_creating_add_on", {
+        p_add_on_name: patch_name
+      });
+      if (is_creating.data) {
+        throw new Error(`There is already an ${patch_name} add-on being created`);
+      }
+      const trainerConfig = {
+        dataset,
+        patch_name,
+        description: args?.description || "",
+        learning_rate: args?.learning_rate || 1e-4,
+        steps: args?.steps || 100,
+        rank: args?.rank || 4
+      };
+      const response = await this.postJob(service_name, trainerConfig);
+      return response;
+    };
+    this.shareAddOn = async (add_on_name, app_user_external_id) => {
+      const my_add_on = this.add_ons.find(
+        (add_on) => add_on.name === add_on_name
+      );
+      if (!my_add_on) {
+        throw new Error(`The add-on ${add_on_name} does not exist`);
+      }
+      const { data, error } = await this.rpc("app_user_share_add_on", {
+        p_add_on_id: my_add_on.id,
+        p_app_user_external_id: app_user_external_id
+      });
+      if (error) {
+        this.handle_error(error);
+      }
+      return data;
+    };
+    this.deleteAddOn = async (add_on_name) => {
+      const my_add_on = this.add_ons.find(
+        (add_on) => add_on.name === add_on_name
+      );
+      if (!my_add_on) {
+        throw new Error(`The add-on ${add_on_name} does not exist`);
+      }
+      const { data, error } = await this.rpc("app_user_delete_add_on", {
+        p_add_on_id: my_add_on.id
+      });
+      if (error) {
+        this.handle_error(error);
+      }
+      return data;
+    };
+    this.renameAddOn = async (add_on_name, new_add_on_name) => {
+      const my_add_on = this.add_ons.find(
+        (add_on) => add_on.name === add_on_name
+      );
+      if (!my_add_on) {
+        throw new Error(`The add-on ${add_on_name} does not exist`);
+      }
+      if (this.add_ons.find((add_on) => add_on.name === new_add_on_name)) {
+        throw new Error(`The add-on ${new_add_on_name} already exists`);
+      }
+      let is_creating = await this.rpc("app_user_is_creating_add_on", {
+        p_add_on_name: new_add_on_name
+      });
+      if (is_creating.data) {
+        throw new Error(`There is already an ${new_add_on_name} add-on being created`);
+      }
+      const { data, error } = await this.rpc("app_user_rename_add_on", {
+        p_add_on_id: my_add_on.id,
+        p_new_name: new_add_on_name
+      });
+      await this.getAddOnList();
+      if (error) {
+        this.handle_error(error);
+      }
+      return data;
+    };
+    this.getCountActiveWorker = async () => {
+      const { data, error } = await this.supabase.rpc("get_active_worker_count", {
+        p_worker_filter: this.worker_filter
+      });
+      if (error) {
+        this.handle_error(error);
+      }
+      return data;
     };
     this.supabase = supabase;
     this.app_id = app_id;
     this.key = key;
     this.app_user_external_id = app_user_external_id;
     this.app_user_token = app_user_token;
-    this.worker_filter = worker_filter || { branch: "prod" };
+    this.worker_filter = worker_filter || { branch: "main" };
     this.app_user_id = "";
     this.services = [];
     this.add_ons = [];
